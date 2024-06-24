5020392c72f582b44461b45cf8e51d9a6256b28d
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2024-06-24 17:19:56 +0530

    Build: Created home page (front-end)

---
+++ components/SavedQutationItem.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableHighlight } from "react-native";
+
+import Colors from "@/constants/Colors";
+import { AntDesign } from "@expo/vector-icons";
+
+const SavedQutationItem = () => {
+  return (
+    <View style={styles.qutationItem}>
+      <View style={styles.qutationItemLeft}>
+        <Text style={styles.qutationItemTitle}>For: Dinujaya Sandaruwan</Text>
+        <Text style={styles.qutationItemDate}>Date: 2024/06/22</Text>
+        <Text style={styles.qutationItemBudget}>Budget: Rs. 57000.00 </Text>
+      </View>
+      <TouchableHighlight
+        onPress={() => console.warn("Saman")}
+        style={styles.qutationItemRight}
+      >
+        <AntDesign name="rightcircleo" size={30} color={Colors.rightArrow} />
+      </TouchableHighlight>
+    </View>
+  );
+};
+
+export default SavedQutationItem;
+
+const styles = StyleSheet.create({
+  qutationItem: {
+    width: "100%",
+    backgroundColor: Colors.darkBg,
+    borderRadius: 10,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: Colors.componentBorder,
+    overflow: "hidden",
+    height: 100,
+
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  qutationItemLeft: {
+    padding: 15,
+  },
+  qutationItemTitle: {
+    color: Colors.white,
+    fontWeight: "700",
+    fontSize: 13,
+  },
+  qutationItemDate: {
+    color: Colors.border,
+    fontWeight: "400",
+    fontSize: 12,
+    marginTop: 3,
+  },
+  qutationItemBudget: {
+    color: Colors.white,
+    fontWeight: "900",
+    fontSize: 18,
+    marginTop: 7,
+  },
+  qutationItemRight: {
+    backgroundColor: Colors.componentBg,
+    height: "100%",
+    width: "20%",
+    justifyContent: "center",
+    alignItems: "center",
+
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: Colors.componentBorder,
+  },
+});
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,14 +1,79 @@
 import NavSearch from "@/components/NavSearch";
+import SavedQutationItem from "@/components/SavedQutationItem";
+import Colors from "@/constants/Colors";
+import { AntDesign } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-import { Button, Text, TouchableOpacity, View } from "react-native";
+import {
+  FlatList,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} from "react-native";
 
 function HomeScreen() {
   const navigation = useNavigation();
   return (
     <>
       <NavSearch />
+      <KeyboardAvoidingView style={styles.container}>
+        <FlatList
+          data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+          ListHeaderComponent={() => (
+            <Text style={styles.title}>Saved Qutations</Text>
+          )}
+          renderItem={() => <SavedQutationItem />}
+          ListFooterComponent={() => <View style={{ height: 55 }} />}
+          ItemSeparatorComponent={() => <View style={{ height: 15 }} />}
+          keyExtractor={(item, index) => index.toString()}
+          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
+          initialNumToRender={5}
+        />
+      </KeyboardAvoidingView>
+      <View style={styles.footer}>
+        <TouchableHighlight
+          style={styles.createBtn}
+          onPress={() => navigation.navigate("Create")}
+        >
+          <AntDesign name="plus" size={50} color={Colors.white} />
+        </TouchableHighlight>
+      </View>
     </>
   );
 }
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 15,
+    backgroundColor: Colors.background,
+  },
+  title: {
+    color: Colors.white,
+    fontWeight: "700",
+    fontSize: 24,
+    marginBottom: 20,
+    paddingTop: 25,
+  },
+  createBtn: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: Colors.componentBg,
+    marginTop: -50,
+    borderWidth: 8,
+    borderColor: Colors.background,
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  footer: {
+    height: 63,
+    backgroundColor: "#151515",
+    alignItems: "center",
+  },
+});
